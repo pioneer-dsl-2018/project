3d73ad810f74beca6a464e58adb263a6ddc15c6f
Teo <zengwc.teo2016@outlook.com>
2018-06-24 22:27:52 -0700

    first version

--- code/Main.py
+++ code/Main.py
@@ -5,17 +5,19 @@ from element import *
 
 # This 'library' converts names to SchemeDraw library names
 # There should be more elements this Dictionary
-library = {'Capacitor':'CAP','Resistor':'RES', 'Cell': 'BAT_CELL','Battery':'BATTERY', 'Dot':'DOT','Switch':'SWITCH_SPST'}
+library = {'Capacitor':'e.CAP','Resistor':'e.RES', 'Cell': 'e.BAT_CELL','Battery':'e.BATTERY', 'Dot':'e.DOT','Switch':'e.SWITCH_SPST'}
 
 # Main
 # In this demo, there is a circuit with one line. And on this line there is an element "capacitor"
 c = circuit()
 l1 = line()
-cap = element('Capacitor',"13nC")
+cap = element(library['Capacitor'],"13nC")
 l1.addElement(cap)
+c.connectInSeries(l1)
+
 
 # replace the argument as the address of the Run.txt file
 # After executing Main.py, just use python to run 'Run.txt'
-c.writeDocument('/Users/teo/Documents/PycharmProjects/Pioneer DSL/Run.txt','output.png')
+c.writeDocument('/Users/teo/Documents/PycharmProjects/Pioneer DSL/Run.txt',"output.png")
 
 
--- code/element.py
+++ code/element.py
@@ -1,13 +1,11 @@
 # The element class
 # There should be private variables such as label and names
 
-
 class element:
 
     # Constructors
-
     def __init__(self, name='', label=''):
-        self.name = name #name is the element itself
+        self.name = name #name is the element itself WITH THE SCHEMDRAW REFERENCE. Instead of 'capacitor', it should be 'e.CAP'
         self.label = label #label is what the user want to be appear on the circuit, it may contains values e.g. 13nC
 
     # when the object is printed, it automatically returns a partial SchemeDraw program
@@ -18,6 +16,7 @@ class element:
     '''
 
     def __str__(self):
-        str = ''
+        str = 'd.add(' + self.name + ',' + 'd = \'right\',' + 'label = ' + '\'' +self.label + '\'' + ')'
+        return str
     #change the name of the element
 
