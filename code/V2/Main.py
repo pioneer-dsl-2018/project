93e66011d7780487c7012195f35263e7950340d3
Teo <zengwc.teo2016@outlook.com>
2018-06-29 22:50:24 -0700

    structure

--- code/V2/Main.py
+++ code/V2/Main.py
@@ -11,16 +11,14 @@ library = {'Capacitor':'e.CAP','Resistor':'e.RES', 'Cell': 'e.BAT_CELL','Battery
 
 # Main
 # In this demo, there is a circuit with one line. And on this line there is an element "capacitor"
-c = circuit()                              # instantiate a new circuit
-l1 = line()                                # instantiate a new line
-cap = element(library['Capacitor'],"13nC") # instantiate a new element and convert the name to a SchemDraw Library name
-l1.addElement(cap)                         # add the capacitor element to l1
+'''
+c = circuit()                                # instantiate a new circuit
+l1 = line()                                  # instantiate a new line
+cap = element(library['Capacitor'],"13nC")   # instantiate a new element and convert the name to a SchemDraw Library name
+l1.addElement(cap)                           # add the capacitor element to l1
 c.connectInSeries(l1)
-
-
 c.evaluate("output.pdf")
-
-# static methods
+'''
 
 
 
