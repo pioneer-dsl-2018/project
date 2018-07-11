56dedb697a195ff5221654ce9d902a6bc8febc53
Teo <zengwc.teo2016@outlook.com>
2018-07-11 15:05:06 -0700

    updated grammar(with new labeling features), but it is not working

--- code/parser.py
+++ code/parser.py
@@ -5,7 +5,7 @@ from element import *
 
 grammar = open('grammar.txt','r').read()
 
-parser = Lark(grammar, start='sentence', ambiguity='explicit')
+parser = Lark(grammar, start='connection')
 
 file = open('input.txt','r')
 program = file.read()
@@ -15,33 +15,13 @@ print(parser.parse(program).pretty())
 
 def run_circuit(program):
     parse_tree = parser.parse(program)
-    for sentence in parse_tree.children:
-        run_sentence(sentence)
+    for child in parse_tree.children:
+        run(child)
 
-def run_sentence(p):
-    if p.data == 'verb':
-        if p.children[0] == 'connect':
-            global l
-            l = line()
-    elif p.data == 'connection':
-        if p.children[0] == 'series':
-            c.connectInSeries(l)
-        elif p.children[0] == 'parellel':
-            c.connectInParallel(l)
-    elif p.data == "conj":
-        if p.children[0] == 'and':
-            pass
-    elif p.data == 'prep':
-        pass
-    elif p.data == 'noun':
-        if p.children[0] == 'capacitor':
-            cap = element('e.CAP', "19C")
-            l.addElement(cap)
-        elif p.children[0] == 'resistor':
-            res = element('e.RES', "89\Omega")
-            l.addElement(res)
+#def run(p):
+
 
 #Main
-c = circuit()
-run_circuit(program)
-c.evaluate("output.png")
+#c = circuit()
+#run_circuit(program)
+#c.evaluate("output.png")
