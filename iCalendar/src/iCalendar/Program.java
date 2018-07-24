8a7e0b4c876b276f6a4d50db9d44f00bc0740533
Jerry-Hung <37008151+Jerry-Hung@users.noreply.github.com>
2018-07-24 10:06:28 -0700

    Update Program.java

--- iCalendar/src/iCalendar/Program.java
+++ iCalendar/src/iCalendar/Program.java
@@ -3,6 +3,7 @@ package iCalendar;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.util.TimeZone;
@@ -24,25 +25,125 @@ public class Program {
 	
 	public static void main(String[] args) throws ParseException {
 		
+		System.out.println("For convenience, please store all text files inside the res folder.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the file name: ");
+		String fileName = sc.nextLine();
+		sc.close();
+		
 		cal.getProperties().add(new ProdId("-//Events Calendar//iCal4j 2.0//EN"));
 		cal.getProperties().add(CalScale.GREGORIAN);
 		Scanner s;
-		try {
-			s = new Scanner(new File("res/dfghj.txt"));
-			while (s.hasNextLine()) {
-				parseEvent(s.nextLine());
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//Scanner s = new Scanner("Robotics meet: Sturts at 5:30 p.m. Ends at 8:00 p.m. Beginning date: 8/31/2018");
 		
+			try {
+				s = new Scanner(new File("res/" + fileName + ".txt"));
+				while (s.hasNextLine()) {
+					parse(s.nextLine());
+				}
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+			
 		System.out.println(cal.toString());
 	}
 	
+	private static void parse(String str) {
+		String[] splited = str.split("\\s+");
+		int index = 0;
+		ArrayList <String> str2 = new ArrayList<String>(splited.length);
+		String test = "";
+		
+		for (int i = 0; i < splited.length; i++ ) {
+			str2.add(splited[i]);
+		}
+		
+		for (int i = 0; i < splited.length; i++) {
+			if (splited[i].charAt(splited[i].length() - 1) == ':') {
+				index = i;
+				break;
+			}
+		}
+		
+		
+		
+		if (splited.length - index == 12) {
+			if(!(splited[index + 1].equalsIgnoreCase("Starts") || (splited[index + 2].equalsIgnoreCase("at")) || (splited[index + 5].equalsIgnoreCase("Ends")) || (splited[index + 6].equalsIgnoreCase("at")) || (splited[index + 9].equalsIgnoreCase("Beginning")) || (splited[index + 10].equalsIgnoreCase("date:")))) {
+				
+				str2.set(index + 1, "Starts");
+				str2.set(index + 2, "at");
+				str2.set(index + 5, "Ends");
+				str2.set(index + 6, "at");
+				str2.set(index + 9, "Beginning");
+				str2.set(index + 10, "date:");
+				for (int i = 0; i < str2.size(); i++) {
+					test += str2.get(i) + " ";
+				}
+				
+				System.out.println("You have entered: {" + str + "}");
+				System.out.println("Do you mean: {" + test + "}? (Yes or No)");
+				Scanner check = new Scanner(System.in);
+				String ans = check.nextLine();
+				check.close();
+				if (ans.equalsIgnoreCase("yes")) {
+					parseEvent(test);
+				} else {
+					System.out.println("Please revise the input string to fit one of the 2 formats supported.");
+					System.out.println("Format 1:{Event name: Starts at _:__ _.m. Ends at _:__ _.m. Beginning date: _/__/____}");
+					System.out.println("Format 2:{Event name: Starts at _:__ Ends at _:__ Beginning date: _/__/____}");
+					System.out.println("Program ending...");
+					System.exit(0);
+				}
+				
+			} else {
+				parseEvent(str);
+			}
+							
+			
+		} else if (splited.length - index == 10) {
+			if(!(splited[index + 1].equalsIgnoreCase("Starts") || (splited[index + 2].equalsIgnoreCase("at")) || (splited[index + 4].equalsIgnoreCase("Ends")) || (splited[index + 5].equalsIgnoreCase("at")) || (splited[index + 7].equalsIgnoreCase("Beginning")) || (splited[index + 8].equalsIgnoreCase("date:")))) {
+				
+				str2.set(index + 1, "Starts");
+				str2.set(index + 2, "at");
+				str2.set(index + 4, "Ends");
+				str2.set(index + 5, "at");
+				str2.set(index + 7, "Beginning");
+				str2.set(index + 8, "date:");
+				for (int i = 0; i < str2.size(); i++) {
+					test += str2.get(i) + " ";
+				}
+				
+				System.out.println("You have entered: {" + str + "}");
+				System.out.println("Do you mean: {" + test + "}");
+				Scanner check2 = new Scanner(System.in);
+				String ans = check2.nextLine();
+				check2.close();
+				if (ans.equalsIgnoreCase("yes")) {
+					parseEvent(test);
+				} else {
+					System.out.println("Please revise the input string to fit one of the 2 formats supported.");
+					System.out.println("Format 1:{Event name: Starts at _:__ _.m. Ends at _:__ _.m. Beginning date: _/__/____}");
+					System.out.println("Format 2:{Event name: Starts at _:__ Ends at _:__ Beginning date: _/__/____}");
+					System.out.println("Program ending...");
+					System.exit(0);
+				}
+				
+			} else {
+				parseEvent1(str);
+			}
+		} else {
+			System.out.println("Invalid file format- Please set the input strings in 1 of 2 formats:");
+			System.out.println("Format 1:{Event name: Starts at _:__ _.m. Ends at _:__ _.m. Beginning date: _/__/____}");
+			System.out.println("Format 2:{Event name: Starts at _:__ Ends at _:__ Beginning date: _/__/____}");
+			System.out.println("Program ending...");
+			System.exit(0);
+		}
+		
+		
+	}
+	
 	private static void parseEvent(String str) {
-		//String str = "Robotics meet: Sturts at 5:30 p.m. Ends at 8:00 p.m. Beginning date: 8/31/2018";
+		//String str = "Robotics meet: Sturts at 5:30 Ends at 8:00 Beginning date: 8/31/2018";
 		TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
 		TimeZone timezone = registry.getTimeZone("America/Mexico_City");
 		VTimeZone tz = ((net.fortuna.ical4j.model.TimeZone) timezone).getVTimeZone();
@@ -64,9 +165,7 @@ public class Program {
 		
 		String description = text.substring(0, text.length() - 2);
 	
-		if (!splited[index + 1].equalsIgnoreCase("Starts")) {
-			System.out.println("you have entered " + splited[index + 1] + ", do you mean Starts?");
-		}
+		
 		int startHour = Integer.parseInt("" + splited[index + 3].charAt(0));
 		int startMin = Integer.parseInt(splited[index + 3].substring(2, 4));
 		
@@ -107,54 +206,63 @@ public class Program {
 	
 	
 	private static void parseEvent1(String str) {
+		
+		//str = "Robotics meet: Sturts at 5:30 Ends at 8:00 Beginning date: 8/31/2018";
 		TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
 		TimeZone timezone = registry.getTimeZone("America/Mexico_City");
 		VTimeZone tz = ((net.fortuna.ical4j.model.TimeZone) timezone).getVTimeZone();
-
-		 // Start Date is on: April 1, 2008, 9:00 am
-		java.util.Calendar startDate = new GregorianCalendar();
-		startDate.setTimeZone(timezone);
-		startDate.set(java.util.Calendar.MONTH, java.util.Calendar.APRIL);
-		startDate.set(java.util.Calendar.DAY_OF_MONTH, 1);
-		startDate.set(java.util.Calendar.YEAR, 2008);
-		startDate.set(java.util.Calendar.HOUR_OF_DAY, 9);
-		startDate.set(java.util.Calendar.MINUTE, 0);
-		startDate.set(java.util.Calendar.SECOND, 0);
-
-		 // End Date is on: April 1, 2008, 13:00
-		java.util.Calendar endDate = new GregorianCalendar();
-		endDate.setTimeZone(timezone);
-		endDate.set(java.util.Calendar.MONTH, java.util.Calendar.APRIL);
-		endDate.set(java.util.Calendar.DAY_OF_MONTH, 1);
-		endDate.set(java.util.Calendar.YEAR, 2008);
-		endDate.set(java.util.Calendar.HOUR_OF_DAY, 13);
-		endDate.set(java.util.Calendar.MINUTE, 0);	
-		endDate.set(java.util.Calendar.SECOND, 0);
 		
-		java.util.Calendar end2 = new GregorianCalendar();
-		end2.clear();
-		end2.set(2008, 4, 1, 13, 0);
-
-		// Create the event
-		String eventName = "Progress Meeting";
-		DateTime start = new DateTime(startDate.getTime());
-		DateTime end = new DateTime(end2.getTime());
-		VEvent meeting = new VEvent(start, end, eventName);
-
-		// add timezone info..
-		//meeting.getProperties().add(tz.getTimeZoneId());
-
+		String[] splited = str.split("\\s+");
 		
-
-		// Create a calendar
-		net.fortuna.ical4j.model.Calendar icsCalendar = new net.fortuna.ical4j.model.Calendar();
-		icsCalendar.getProperties().add(new ProdId("-//Events Calendar//iCal4j 1.0//EN"));
-		icsCalendar.getProperties().add(CalScale.GREGORIAN);
-
-
-		// Add the event and print
-		icsCalendar.getComponents().add(meeting);
-		System.out.println(icsCalendar);
+		int index = 0;
+		String text = "";
+		for (int i = 0; i < splited.length; i++) {
+			if (splited[i].charAt(splited[i].length() - 1) == ':') {
+				index = i;
+				break;
+			}
+		}
+		for (int i = 0; i <= index; i++) {
+			text += splited[i];
+			text += " ";
+		}
+		
+		String description = text.substring(0, text.length() - 2);
+		
+		Scanner startSc = new Scanner(splited[index + 3]);
+		 startSc.findInLine("(\\d+):(\\d+)");
+	     MatchResult sResult = startSc.match();
+	     int startHour = Integer.parseInt(sResult.group(1));
+		 int startMin = Integer.parseInt(sResult.group(2));
+	   
+	     startSc.close(); 
+	
+	     Scanner endSc = new Scanner(splited[index + 6]);
+		 endSc.findInLine("(\\d+):(\\d+)");
+	     MatchResult eResult = endSc.match();
+	     int endHour = Integer.parseInt(eResult.group(1));
+		 int endMin = Integer.parseInt(eResult.group(2));
+	   
+	     endSc.close(); 
+		
+	
+		
+		
+		Scanner sc = new Scanner(splited[splited.length - 1]);
+		 sc.findInLine("(\\d+)/(\\d+)/(\\d+)");
+	     MatchResult result = sc.match();
+	     int month = Integer.parseInt(result.group(1));
+		 int day = Integer.parseInt(result.group(2));
+	     int year = Integer.parseInt(result.group(3));
+	     sc.close(); 
+	     
+	     java.util.Calendar start = new dateTime(month, day, year, startHour, startMin).getTime();
+	     java.util.Calendar end = new dateTime(month, day, year, endHour, endMin).getTime();
+	     
+	     DateTime startDT = new DateTime(start.getTime());
+	     DateTime endDT = new DateTime(end.getTime());
+	     
+	     cal.getComponents().add(new CEvent(startDT, endDT, description));
 	}
 }
 
